1ed48619f3465e27309ece274d18946f8163f5b7
Abolfazl <iabolfazl83@yahoo.com>
2026-07-04 23:01:48 +0330

    completed App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,15 @@
+import {AddHabitForm} from "./components/AddHabitForm.tsx";
+import {useHabits} from "./hooks/useHabits.ts";
+import {HabitList} from "./components/HabitList.tsx";
+
 function App() {
+    const {addHabit, activeHabits, completeHabit, deleteHabit} = useHabits();
 
     return (
-        <></>
+        <>
+            <AddHabitForm onAddHabit={addHabit}/>
+            <HabitList habits={activeHabits} onComplete={completeHabit} onDelete={deleteHabit}/>
+        </>
     )
 }
 
