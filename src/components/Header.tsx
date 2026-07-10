58816c405612ae80b84a20d06dfdcbf13e6dc3b4
Abolfazl <iabolfazl83@yahoo.com>
2026-07-11 00:40:49 +0330

    updated Header.tsx

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -3,7 +3,7 @@ import {ThemeToggle} from "./ThemeToggle.tsx";
 export function Header() {
     return (
         <header className="w-full">
-            <div className="my-4 flex justify-between items-center gap-2">
+            <div className="flex justify-between items-center gap-2">
                 <div>
                     <h1 className="text-claude-text dark:text-[#c3c2b7] font-bold text-2xl">Habits</h1>
                     <p className="text-claude-mute-text">Track what matters, every day.</p>
