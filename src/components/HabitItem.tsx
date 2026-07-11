6f6627c6bb920ee47cbd8487974f2bcd4336143e
Abolfazl <iabolfazl83@yahoo.com>
2026-07-12 01:48:49 +0330

    added editing future to HabitItem.tsx

--- src/components/HabitItem.tsx
+++ src/components/HabitItem.tsx
@@ -1,36 +1,88 @@
 import type {Habit} from "../types/habits.ts";
 import {Checkbox} from "./Checkbox.tsx";
-import {IconTrash} from "@tabler/icons-react";
+import {IconPencil, IconTrash} from "@tabler/icons-react";
+import {type ChangeEvent, type SubmitEvent, useRef, useState} from "react";
 
 interface HabitItemProps {
     habit: Habit;
     onComplete: (id: string) => void;
     onDelete: (id: string) => void;
+    onEdit: (name: string, id: string) => void;
 }
 
 
-export function HabitItem({habit, onComplete, onDelete}: HabitItemProps) {
+export function HabitItem({habit, onComplete, onDelete, onEdit}: HabitItemProps) {
     const today = new Date().toISOString().split("T")[0];
     const isCompletedToday = habit.completedDates.includes(today);
     const isMobile = /Android|iPhone|iPad|iPod/i.test(navigator.userAgent);
+    const [habitName, setHabitName] = useState(habit.name);
+    const [isEditing, setIsEditing] = useState(false);
+    const inputRef = useRef<HTMLInputElement>(null);
+    const formRef = useRef<HTMLFormElement>(null);
+    const [error, setError] = useState(false);
+
+    const handleEditClick = () => {
+        if (!isEditing) {
+            setIsEditing(true);
+            inputRef.current?.focus();
+        } else {
+            handleSubmit()
+        }
+    };
+
+    const handleHabitChange = (e: ChangeEvent<HTMLInputElement>) => {
+        setHabitName(e.target.value);
+        setError(false);
+    }
+
+    const handleSubmit = (event?: SubmitEvent<HTMLFormElement>) => {
+        event?.preventDefault();
+        const trimmedName = habitName.trim();
+        if (trimmedName.length < 1) {
+            setError(true);
+            inputRef.current?.focus();
+            return;
+        }
+
+        onEdit(trimmedName, habit.id);
+        setIsEditing(false);
+        setError(false);
+        inputRef.current?.blur();
+    };
+
     return (
-        <li className={`${isCompletedToday ? "opacity-50" : ""} flex justify-between items-center group px-6 py-4 my-2 bg-claude-input border-claude-border hover:border-stone-400 dark:bg-claude-input-dark rounded-xl border dark:border-claude-border-dark hover:dark:border-stone-600 text-claude-text dark:text-claude-text-dark duration-200`}>
-            <div className="flex gap-4 items-center">
-                <Checkbox
-                    checked={isCompletedToday}
-                    onChange={() => onComplete(habit.id)}
-                    label={`Mark ${habit.name} as ${isCompletedToday ? "incomplete" : "complete"}`}
-                />
-                {isCompletedToday ? <s>{habit.name}</s> : <span>{habit.name}</span>}
+        <li className={`${isCompletedToday ? "opacity-50" : ""} flex gap-2 justify-between items-center group px-6 py-4 my-2 bg-claude-input border-claude-border hover:border-stone-400 dark:bg-claude-input-dark rounded-2xl border dark:border-claude-border-dark hover:dark:border-stone-600 text-claude-text dark:text-claude-text-dark duration-200`}>
+            <div className="flex gap-4 items-center flex-1">
+                {!isEditing && (
+                    <Checkbox
+                        checked={isCompletedToday}
+                        onChange={() => onComplete(habit.id)}
+                        label={`Mark ${habit.name} as ${isCompletedToday ? "incomplete" : "complete"}`}
+                    />
+                )}
+                <form ref={formRef} onSubmit={handleSubmit}>
+                    <input type="text"
+                           className={`w-full ${isCompletedToday ? "line-through" : ""} ${isEditing ? "border-claude-border dark:border-claude-border-dark focus:border-stone-400 focus:dark:border-stone-500" : "border-transparent"} ${error ? "border-red-400!" : ""} border outline-0 rounded-xl p-2 duration-200`}
+                           ref={inputRef} value={habitName} onChange={handleHabitChange} disabled={!isEditing}/>
+                </form>
             </div>
             <div>
-                <button
-                    className={`${isMobile ? "opacity-100" : ""} opacity-0 group-hover:opacity-100 duration-200 hover:cursor-pointer`}
-                    type="button"
-                    onClick={() => onDelete(habit.id)}>
+                {!isEditing && (
+                    <button
+                        className={`${isMobile ? "opacity-100" : ""} opacity-0 group-hover:opacity-100 duration-200 hover:cursor-pointer`}
+                        type="button"
+                        onClick={() => onDelete(habit.id)}>
+                        <div
+                            className="text-red-700 hover:bg-red-700 active:bg-red-700 dark:text-red-400 active:dark:bg-red-700 active:text-white hover:text-white p-1 rounded-md duration-200">
+                            <IconTrash stroke={1}/>
+                        </div>
+                    </button>
+                )}
+                <button onClick={handleEditClick}
+                        className={`${isMobile ? "opacity-100" : ""} opacity-0 group-hover:opacity-100 duration-200 hover:cursor-pointer`}>
                     <div
-                        className="text-red-700 hover:bg-red-700 active:bg-red-700 dark:text-red-400 active:dark:bg-red-700 active:text-white hover:text-white p-1 rounded-md duration-200">
-                        <IconTrash stroke={1}/>
+                        className="text-claude-text cursor-pointer dark:text-claude-text-dark p-1 rounded-md duration-200">
+                        <IconPencil stroke={1}/>
                     </div>
                 </button>
             </div>
